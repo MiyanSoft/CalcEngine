69dc0b5620ba6c000867fd17f1b6995a51b92e51
Blessing <ahisublessing@gmail.com>
2020-08-10 14:32:26 -0700

    After adding static members, before implementing method overloading

--- src/com/miyansoft/calcengine/Main.java
+++ src/com/miyansoft/calcengine/Main.java
@@ -12,25 +12,20 @@ public class Main {
      static void performCalculation() {
 
         MathEquation[] equations = new MathEquation[4]; //creates 4 references of type MathEquation (each element of this array will create the instance of the MA class)
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
         for(MathEquation equation: equations)   {
             equation.execute();
             System.out.println("result = " + equation.result);
 
         }
+        System.out.println("Average result = " + MathEquation.getAverageResult());  //since getAverageResult is a static method, it's been accessed using MathEquation classname
 
     }
 
-    private static MathEquation create(double leftVals, double rightVals, char opCode) {
-        MathEquation equation = new MathEquation(); //create new instance of ME class
-        equation.leftVal = leftVals;
-        equation.rightVal = rightVals;
-        equation.opCode = opCode;
-        return equation;                            //returns the reference of the newly created ME class
-    }
+
 
 }
--- src/com/miyansoft/calcengine/MathEquation.java
+++ src/com/miyansoft/calcengine/MathEquation.java
@@ -6,7 +6,27 @@ public class MathEquation {
     char opCode;
     double result;
 
+    //provide average result of the calculations for all MathEquation instances in a way that's not tied to any instance of this class  using static members
+    private static int numberOFCalculations;
+    private static double sumOfResults;
+
+    public MathEquation() { }
+
+    public MathEquation(char opCode)    {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal)   {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+
+    }
+
+
     void execute()  {
+
         switch(opCode)	{
             case 'a':
                 result= leftVal + rightVal;
@@ -26,5 +46,42 @@ public class MathEquation {
                 result = 0.0d;
                 break;
         }
+        //keep count of the number of calculations executed as well as the total of all the results
+        numberOFCalculations++;
+        sumOfResults += result;
+
     }
+
+    public static double getAverageResult(){
+     return sumOfResults / numberOFCalculations;
+    }
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
 }
